75327ac845caceadb264ecb24da0c4001598fdfe
DaryaSerikova <dvserikova@gmail.com>
2022-05-25 01:55:24 +0700

    add editing message

--- src/components/Panel/Panel.js
+++ src/components/Panel/Panel.js
@@ -63,26 +63,30 @@ const Panel = ({
 
 
   return (
-    <form ref={formEl} className='panel' onSubmit={onSubmit}  >
-    
-      <textarea 
-        className='textarea' 
-        placeholder='Write message..' 
-        // value={mapStateToProps.currentMessage && mapStateToProps.currentMessage}
-        value={currentMessage}
-        onChange={(e) => updateToNewCurrentMessage(e.target.value)}
-        onKeyPress={onKeyPressEnter}
-        autoFocus
-      ></textarea>
+    <>
+      <form ref={formEl} className='panel' onSubmit={onSubmit}  >
+        {<span className={`editing-message ${(messageState !== 'edit')&&'hideOpacity'}`}>Editing a message</span>}
+        
+        <textarea 
+          className={`textarea ${(messageState === 'edit')&&'border-editing'}`}
+          placeholder='Write message..' 
+          // value={mapStateToProps.currentMessage && mapStateToProps.currentMessage}
+          value={currentMessage}
+          onChange={(e) => updateToNewCurrentMessage(e.target.value)}
+          onKeyPress={onKeyPressEnter}
+          autoFocus
+        ></textarea>
 
-      <div className='wrapper-btn'>
-        <button 
-          type="submit" 
-          className='btn btn-success custom-btn'
-        > Send </button>
-      </div>
+        <div className='wrapper-btn'>
+          <button 
+            type="submit" 
+            className='btn btn-success custom-btn'
+          > Send </button>
+        </div>
 
-    </form>
+      </form>
+    </>
+
   )
 }
 
